8f9b9ef2e3dc7970ca3da3f35dc77b2a34862915
Hanna Ponomarenko <pnmrnk.anna@gmail.com>
2025-08-01 15:16:55 +0200

    add homework node

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,11 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const generateContacts = async (number) => {
+  const prevContacts = await readContacts();
+  const contacts = Array(number).fill(0).map(createFakeContact);
+  await writeContacts([...prevContacts, ...contacts]);
+};
 
 generateContacts(5);
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,7 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+// export const writeContacts = async (updatedContacts) => {};
+
+export const writeContacts = (contacts) =>
+  fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
